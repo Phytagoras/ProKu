ab06cb50ad9c54cfc3b21a40dfa46af46fa06a17
Pythagoras <36764405+Phytagoras@users.noreply.github.com>
2024-03-01 20:33:56 +0100

    Update teilA.cpp

--- ProKu/Aufgabe1/teilA.cpp
+++ ProKu/Aufgabe1/teilA.cpp
@@ -1,14 +1,16 @@
-#include <iostream> // Bibliothek für Ein-/Ausgabeoperationen
+#include <iostream>  // Bibliothek für Ein-/Ausgabeoperationen
 
 // Funktionsdeklarationen für berechneSumme1 und berechneSumme2
 double berechneSumme1();
 double berechneSumme2();
 
 int main() {
-    // Berechnet die Summe mit der Funktion berechneSumme1 und speichert das Ergebnis in summeX
+    // Berechnet die Summe mit der Funktion berechneSumme1 und speichert das
+    // Ergebnis in summeX
     double summeX = berechneSumme1();
 
-    // Berechnet die Summe erneut mit der Funktion berechneSumme1 und speichert das Ergebnis in summeY
+    // Berechnet die Summe erneut mit der Funktion berechneSumme1 und speichert
+    // das Ergebnis in summeY
     double summeY = berechneSumme2();
 
     // Ausgabe der berechneten Summe X mit einer Beschreibung
@@ -24,27 +26,31 @@ int main() {
     std::cout << "1 - y: " << 1.0 - summeY << std::endl;
 
     // Vergleicht summeX und summeY und gibt das Ergebnis (wahr oder falsch) aus
-    std::cout << "x == y: " << ((summeX == summeY) ? "wahr" : "falsch") << std::endl;
+    std::cout << "x == y: " << ((summeX == summeY) ? "wahr" : "falsch")
+              << std::endl;
 
-    return 0; // Zeigt erfolgreiches Beenden des Programms an
+    return 0;  // Zeigt erfolgreiches Beenden des Programms an
 }
 
 // Funktion zur Berechnung der Summe von 8 Termen, jeder mit dem Wert 1.0 / 8.0
 double berechneSumme1() {
-    double summe = 0.0; // Initialisierung einer Variablen zur Speicherung der Summe
+    double summe =
+        0.0;  // Initialisierung einer Variablen zur Speicherung der Summe
     for (int i = 0; i < 8; i++) {
-        summe += 1.0 / 8.0; // Addiert 1.0 / 8.0 zur Summe in jeder Iteration
+        summe += 1.0 / 8.0;  // Addiert 1.0 / 8.0 zur Summe in jeder Iteration
     }
-    return summe; // Rückgabe der finalen berechneten Summe
+    return summe;  // Rückgabe der finalen berechneten Summe
 }
 
-// Funktion zur Berechnung der Summe von 10 Termen, jeder mit dem Wert 1.0 / 10.0
+// Funktion zur Berechnung der Summe von 10 Termen, jeder mit dem Wert 1.0
+// / 10.0
 double berechneSumme2() {
-    double summe = 0.0; // Initialisierung einer Variablen zur Speicherung der Summe
+    double summe =
+        0.0;  // Initialisierung einer Variablen zur Speicherung der Summe
     for (int i = 0; i < 10; i++) {
-        summe += 1.0 / 10.0; // Addiert 1.0 / 10.0 zur Summe in jeder Iteration
+        summe += 1.0 / 10.0;  // Addiert 1.0 / 10.0 zur Summe in jeder Iteration
     }
-    
-        std::cout << summe << 1.0 - summe << std::endl;
-    return summe; // Rückgabe der finalen berechneten Summe
+
+    std::cout << summe << 1.0 - summe << std::endl;
+    return summe;  // Rückgabe der finalen berechneten Summe
 }
