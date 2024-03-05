7f1d5b2be13c4ba2a5ea0d66403b7466bdfd8d57
Pythagoras <36764405+Phytagoras@users.noreply.github.com>
2024-03-05 17:28:54 +0100

    bisF

--- ProKu/aufgabe9/image.cpp
+++ ProKu/aufgabe9/image.cpp
@@ -3,7 +3,7 @@
 #include <cassert>
 
 #include "jpeg_io.hpp"
-
+typedef unsigned int uint;
 Image::Image() {
     my_height = 0;
     my_width = 0;
@@ -19,5 +19,44 @@ Image::Image(uint width, uint height, const char* name) {
     my_width = width;
     my_pixels = read_jpeg(width, height, name);
 }
+Image::Image(const Image& oldImage) {
+    my_height = oldImage.my_height;
+    my_width = oldImage.my_width;
+    my_pixels = new float[my_height * my_width];
+    for (int i = 0; i < my_height * my_width; i++) {
+        my_pixels[i] = oldImage.my_pixels[i];
+    }
+}
 
 Image::~Image() { delete[] my_pixels; }
+
+Image& Image::operator=(const Image& other) {
+    if (this == &other) return *this;
+
+    if (my_pixels != nullptr) delete[] my_pixels;
+    int length = other.my_height * other.my_width;
+    my_height = other.my_height;
+    my_width = other.my_width;
+    my_pixels = nullptr;
+    if (length > 0) {
+        assert(other.my_pixels != nullptr);
+        my_pixels = new float[length];
+        for (int i(0); i < length; ++i) {
+            my_pixels[i] = other.my_pixels[i];
+        }
+    } else
+        my_pixels = new float[0];
+    return *this;
+}
+uint Image::height() const { return my_height; }
+uint Image::width() const { return my_width; }
+
+float& Image::at(int i, int j) { 
+    assert(0 <= i && my_height >= i);
+    assert(0 <= j && my_width >= j);
+    return my_pixels[my_width * i + j];
+}
+
+bool Image::write(const char* name) const{
+    return write_jpeg(my_width, my_height, my_pixels, name);
+}
--- ProKu/aufgabe9/image.hpp
+++ ProKu/aufgabe9/image.hpp
@@ -9,14 +9,20 @@ class Image {
     // TODO
    private:
     uint my_width, my_height;
-    float *my_pixels;
+    float* my_pixels;
 
    public:
     Image();
     Image(uint width, uint height);
     Image(uint width, uint height, const char* name);
+    Image(const Image& oldImage);
+    Image& operator=(const Image& other);
     ~Image();
+    uint height() const;
+    uint width() const;
 
+    float& Image::at(int i, int j);
+    bool write(const char* name) const;
 };  // class Image
 
 #endif  // PROKU_IMAGE_HPP
